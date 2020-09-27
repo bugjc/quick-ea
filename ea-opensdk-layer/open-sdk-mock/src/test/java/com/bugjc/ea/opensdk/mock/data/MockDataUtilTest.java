fa512fae5796be1b2b43b51c2e05e1473a9f77b0
qing.muyi@foxmail.com <qing.muyi@foxmail.com>
2020-09-27 15:02:46 +0800

    日常提交

--- ea-opensdk-layer/open-sdk-mock/src/test/java/com/bugjc/ea/opensdk/mock/data/MockDataUtilTest.java
+++ ea-opensdk-layer/open-sdk-mock/src/test/java/com/bugjc/ea/opensdk/mock/data/MockDataUtilTest.java
@@ -1,6 +1,7 @@
 package com.bugjc.ea.opensdk.mock.data;
 
 import com.bugjc.ea.opensdk.mock.data.entity.Param;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
 class MockDataUtilTest {
@@ -8,6 +9,6 @@ class MockDataUtilTest {
     @Test
     void mock() {
         Param param = MockDataUtil.mock(Param.class);
-        System.out.println(param.getAByte());
+        System.out.println(new Gson().toJson(param));
     }
 }
